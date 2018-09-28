1dbee75238384116b5106ff6c45ab13b0d01e830
xianhaic <xianhaic@gmail.com>
2018-09-28 02:06:26 -0700

    added more functions

--- macros.h
+++ macros.h
@@ -2,8 +2,11 @@
 #define MACROS_H
 
 #define DECK_SIZE 52
+#define DECK_SUITS 4
+#define SUIT_SIZE 13
 #define PLAYER_STARTING_CARDS 10
 #define DECK_LEFTOVER_WIN 3
 
+#define BUFSIZ_VAL 8
 
 #endif
--- util.c
+++ util.c
@@ -1,10 +1,20 @@
+#include "util.h"
 
 // modifies a string since the value 10 is two chars
-void resolve_card_val(char *val, int card) {
+void resolve_card_val(char *val, int len_val, int card) {
+    int val_index = card % DECK_SIZE;
+    char *vals_all[] = CARD_TO_VAL;
     
+    memset(val, 0, len_val);
+    strncpy(val, vals_all[val_index], strlen(vals_all[val_index]));
 }
 
 // returns the type of the card as a char
-char resolve_card_type(int card) {
+void resolve_card_suit(char *suit, int card) {
+    int suit_index = card / DECK_SUITS;
+    char suits_all[] = CARD_TO_SUIT;
+    
 
+
+    return suits_all[suit_index];
 }
